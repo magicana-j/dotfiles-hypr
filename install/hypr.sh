f28f53e8b047fff9ef76dadd22925fbfc5f950bc
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-13 23:05:18 +0900

    Add files via upload

---
+++ install/hypr.sh
@@ -0,0 +1,16 @@
+yay -S --needed waybar hyprpaper wofi cliphist grimblast brightnessctl hyprshot wlogout
+yay -S --needed ttf-font-awesome #フォント
+
+# yay -S greetd-tuigreet # ログイン画面
+yay -S --needed thunar dolphin thunar gvfs tumbler ffmpegthumbnailer # ファイルマネージャ
+# yay -S foot # ターミナル
+yay -S --needed alacritty
+yay -S --needed gnome-keyring # キーリング
+yay -S --needed htop powertop smartmontools ufw # ユーティリティ
+yay -S --needed hyprlock hypridle # ロック画面 自動ロック
+
+yay -S iwgtk pavucontrol nwg-look-bin gnome-themes-extra qt5ct network-manager-applet # GUI関係
+# yay -S pipewire pipewire-pulse
+yay -S --needed mako xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
+
+yay -S --needed hyprland
