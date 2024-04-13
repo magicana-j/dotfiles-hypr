372afa57ee137c65794ec56b0530c70d5c09694f
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-13 23:02:25 +0900

    Create software.sh

---
+++ install/software.sh
@@ -0,0 +1,6 @@
+sudo -Syyu
+sudo pacman -S --needed vim neovim nano less timeshift neofetch wget curl unzip
+sudo pacman -S --needed ufw gufw xdg-user-dirs-gtk gnome-keyring
+sudo pacman -S --needed firefox
+sudo pacman -S --needed go
+sudo pacman -S --needed noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
